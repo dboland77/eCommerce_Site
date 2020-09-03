c619e7ae9fa942c58539bb16a9791c122d5a18e6
David Boland <dboland77@gmail.com>
2020-09-03 17:05:54 +0100

    Updates

---
+++ precache-manifest.aa6f446a8fc58308304d89d16e69de4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1888cef7d703a03e8bf1b731027e4431",
+    "url": "/eCommerce_Site/index.html"
+  },
+  {
+    "revision": "0898ab1162c6092c45d2",
+    "url": "/eCommerce_Site/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "244b9a9eb93b59ad3644",
+    "url": "/eCommerce_Site/static/css/main.c1c46a86.chunk.css"
+  },
+  {
+    "revision": "0898ab1162c6092c45d2",
+    "url": "/eCommerce_Site/static/js/2.3f025129.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eCommerce_Site/static/js/2.3f025129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244b9a9eb93b59ad3644",
+    "url": "/eCommerce_Site/static/js/main.75fb2174.chunk.js"
+  },
+  {
+    "revision": "0e68ddcfb097a3ec4e31",
+    "url": "/eCommerce_Site/static/js/runtime-main.f05d3a13.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/eCommerce_Site/static/media/logo.9f7e506d.svg"
+  }
+]);
