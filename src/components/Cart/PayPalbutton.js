c8d6ff2f81f4b139c48cb18fe608b5ad13aab498
David Boland <dboland77@gmail.com>
2020-09-05 19:45:26 +0100

    added production env

--- src/components/Cart/PayPalbutton.js
+++ src/components/Cart/PayPalbutton.js
@@ -32,8 +32,8 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: process.env.REACT_APP_CLIENT_ID
-      // production: process.env.CLIENT_ID,
+      sandbox: process.env.REACT_APP_CLIENT_ID,
+      production: process.env.REACT_APP_CLIENT_ID,
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
     // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
